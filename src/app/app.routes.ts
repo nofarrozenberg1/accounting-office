490be56bfecd76a7ea8f477bd4f4a06c143e42ac
nofarrozenberg1 <nofarrozenberg1@gmail.com>
2025-02-07 21:28:45 +0200

    Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ResponsibilitiesComponent } from './pages/responsibilities/responsibilities.component';
+import { LinksComponent } from './pages/links/links.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },  // Home Page
+  { path: 'about', component: AboutComponent },  // About Page
+  { path: 'responsibilities', component: ResponsibilitiesComponent },  // Office Responsibilities
+  { path: 'links', component: LinksComponent },  // Links Page
+  { path: 'contact', component: ContactComponent },  // Contact Us Page
+];
---
+++ src/app/pages/responsibilities/responsibilities.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-responsibilities',
+  imports: [],
+  templateUrl: './responsibilities.component.html',
+  styleUrl: './responsibilities.component.css'
+})
+export class ResponsibilitiesComponent {
+
+}
